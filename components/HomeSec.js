4ba87cc5c414e572b4b91ed99a08acb6317fa510
临风 许 <WarMachine2012@outlook.com>
2023-03-05 22:47:21 -0800

    initial commit

--- components/HomeSec.js
+++ components/HomeSec.js
@@ -6,6 +6,9 @@ export default function HomeSec() {
         <p className="text-xl text-gray-700">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
         <button className="px-10 py-3 bg-amber-400 text-2xl mt-10 rounded-md drop-shadow-2xl hover:bg-amber-500">Projects</button>
       </div>
+      <div>
+        Side panel
+      </div>
     </div>
   );
 }
