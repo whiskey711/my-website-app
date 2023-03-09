94ea14423580f297688c57dbc9b15a8a286b6378
临风 许 <WarMachine2012@outlook.com>
2023-03-08 21:05:51 -0800

    home sec completed

--- components/HomeSec.js
+++ components/HomeSec.js
@@ -1,13 +1,22 @@
-import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineFileText, AiOutlineWechat } from "react-icons/ai";
-//import myResume from "../public/myResume.pdf";
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineFileText, AiOutlineMail } from "react-icons/ai";
+import Router from "next/router";
 
 export default function HomeSec() {
+
+  function gotoProjects() {
+    Router.push("#projects");
+  }
+
   return (
     <div className="bg-stone-100 min-h-screen flex justify-center items-center">
-      <div className="border border-teal-500 relative mx-40 text-center">
+      <div className="relative mx-40 text-center">
         <h1 className="text-6xl font-black uppercase">Hi, I am Linfeng Xu</h1>
         <p className="text-xl text-gray-700 mt-4 mb-8">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
-        <button className="px-10 py-3 bg-amber-400 text-2xl rounded-md drop-shadow-xl hover:bg-amber-500 hover:scale-110 transition">Projects</button>
+        <button 
+          className="px-10 py-3 bg-amber-400 text-2xl rounded-md drop-shadow-xl hover:bg-amber-500 hover:scale-110 transition"
+          onClick={gotoProjects}>
+            Projects
+        </button>
       </div>
       <div className="drop-shadow-xl rounded-sm absolute right-1 bg-neutral-100 p-2 space-y-2">
         <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
@@ -30,10 +39,9 @@ export default function HomeSec() {
         </div>
         <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
           <a 
-            target="_blank" 
-            rel="noopener noreferrer"
-            title="WeChat">
-            <AiOutlineWechat size={34} />
+            href="mailto:james2012.shun@gmail.com"
+            title="Email">
+            <AiOutlineMail size={34} />
           </a>
         </div>
         <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
