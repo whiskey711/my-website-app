6109c65cd7af2c25f88e0f14a722adf7a4392294
James <linfengx@uvic.ca>
2023-03-09 16:20:48 -0800

    add no function form to submit message

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,22 @@
 export default function Contact() {
   return (
-    <div className="border border-green-500 min-h-screen">
-      Contact
+    <div className="border border-green-500 min-h-screen flex flex-col justify-center items-center">
+      <h1>Contact</h1>
+      <form className="flex flex-col space-y-5 bg-orange-100 p-5">
+        <div className="space-x-3">
+          <input 
+            type="text" 
+            className="contactInput"
+            placeholder="Name"></input>
+          <input 
+            type="email" 
+            className="contactInput"
+            placeholder="Email"></input>
+        </div>
+        
+        <textarea className="contactInput" placeholder="Message"></textarea>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
