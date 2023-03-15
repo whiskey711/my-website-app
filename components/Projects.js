76728938326f5f54bd7ede9d0dc53a2eb62b2428
临风 许 <WarMachine2012@outlook.com>
2023-03-14 22:00:38 -0700

    image display normally

--- components/Projects.js
+++ components/Projects.js
@@ -6,7 +6,7 @@ import tenzieScreenShot from "../public/images/tenzies-screenshot.png"
 export default function Projects() {
   const projs = [
     {
-      screenShot: "../public/images/tenzies-screenshot.png",
+      screenShot: tenzieScreenShot,
       title: "Tenzies",
       description: "This is tenzies",
       url: "/projects/tenzies"
@@ -16,7 +16,9 @@ export default function Projects() {
   const projElements = projs.map((proj) => {
     return (
       <div key={nanoid()} className="flex-none mr-8 w-2/3 border">
-        <img src={proj.screenShot} alt="tenzies" />
+        <div className="">
+          <Image src={proj.screenShot} alt="tenzies" />
+        </div>
         <h1>{proj.title}</h1>
         <p>{proj.description}</p>
         <Link href={proj.url}>Link</Link>
@@ -28,27 +30,29 @@ export default function Projects() {
       <div className="grid grid-cols-2 gap-5 m-5">
         <Link
           href="/projects/travel-journal"
-          className="bg-zinc-200 hover:cursor-pointer h-32 drop-shadow-md rounded-lg flex justify-center items-center">
+          className="bg-zinc-200 hover:cursor-pointer h-8 drop-shadow-md rounded-lg flex justify-center items-center">
           Travel Journal
         </Link>
         <Link
           href="/projects/meme-generator"
-          className="bg-zinc-200 hover:cursor-pointer h-32 drop-shadow-md rounded-lg flex justify-center items-center">
+          className="bg-zinc-200 hover:cursor-pointer h-8 drop-shadow-md rounded-lg flex justify-center items-center">
           Meme Generator
         </Link>
         <Link
           href="/projects/tenzies"
-          className="bg-zinc-200 hover:cursor-pointer h-32 drop-shadow-md rounded-lg flex justify-center items-center">
+          className="bg-zinc-200 hover:cursor-pointer h-8 drop-shadow-md rounded-lg flex justify-center items-center">
           Tenzies
         </Link>
         <Link
           href="/projects/quizzical"
-          className="bg-zinc-200 hover:cursor-pointer h-32 drop-shadow-md rounded-lg flex justify-center items-center">
+          className="bg-zinc-200 hover:cursor-pointer h-8 drop-shadow-md rounded-lg flex justify-center items-center">
           Quizzical
         </Link>
       </div>
       <div className="flex flex-nowrap overflow-x-scroll items-start mb-8">
         {projElements}
+      </div>
+      <div>
       </div>
     </div>
   );
