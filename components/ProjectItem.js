b9be69b766b2bec15e8062764637d9af53e93bed
临风 许 <WarMachine2012@outlook.com>
2023-03-18 21:13:22 -0700

    change gradient color

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function ProjectItem(props) {
   return (
-    <div className="relative flex w-full h-auto justify-center items-center group rounded-lg shadow-xl p-3 bg-stone-200 hover:bg-gradient-to-r from-orange-300 to-orange-peel transition duration-300">
+    <div className="relative flex w-full h-auto justify-center items-center group rounded-lg shadow-xl p-3 bg-stone-200 hover:bg-gradient-to-br from-orange-peel to-phantone-red transition duration-300">
       <Image src={props.content.screenShot} alt="" className="rounded-lg group-hover:opacity-20 transition duration-300" />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:block transition text-center text-white">
         <h1 className="text-3xl font-bold tracking-wider">{props.content.title}</h1>
