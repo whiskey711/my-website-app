ec68e172d9a20f867f8712c7baa43bba00be1b0b
James <linfengx@uvic.ca>
2023-04-14 17:11:24 -0700

    home section rework done

--- components/HomeSec.js
+++ components/HomeSec.js
@@ -3,54 +3,48 @@ import Router from "next/router";
 
 export default function HomeSec() {
 
-  function gotoProjects() {
-    Router.push("#projects");
-  }
-
   return (
-    <div className="bg-stone-100 min-h-screen flex justify-center items-center">
-      <div className="relative mx-40 text-center">
-        <h1 className="text-6xl font-black uppercase">Hi, I am Linfeng Xu</h1>
-        <p className="text-xl text-gray-700 mt-4 mb-8">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
-        <button 
-          className="px-10 py-3 bg-amber-400 text-2xl rounded-md drop-shadow-xl hover:bg-amber-500 hover:scale-110 transition"
-          onClick={gotoProjects}>
-            Projects
-        </button>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="mx-40 flex flex-col justify-center items-center">
+        <p className="bg-gray-300 px-2 uppercase">Let's build something together</p>
+        <h1 className="text-6xl font-black my-4">
+          Hi, I am <span className="text-sky-400">Linfeng Xu</span>
+        </h1>
+        <p className="text-xl text-gray-700 mt-4 mb-8 text-center">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
       </div>
-      <div className="drop-shadow-xl rounded-sm absolute right-1 bg-neutral-100 p-2 space-y-2">
-        <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
+      <div className="p-4 space-x-5 flex">
+        <div className="rounded-full p-5 shadow-lg cursor-pointer hover:scale-110 hover:bg-blue-100 duration-300">
           <a 
             href="https://github.com/whiskey711" 
             target="_blank" 
             rel="noopener noreferrer"
             title="Github">
-            <AiOutlineGithub size={34} />
+            <AiOutlineGithub size={22} />
           </a>
         </div>
-        <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
+        <div className="rounded-full p-5 shadow-lg cursor-pointer hover:scale-110 hover:bg-blue-100 duration-300">
           <a 
             href="https://www.linkedin.com/in/linfeng-xu-98b51a126/"
             target="_blank" 
             rel="noopener noreferrer"
             title="LinkedIn">
-            <AiOutlineLinkedin size={34} />
+            <AiOutlineLinkedin size={22} />
           </a>
         </div>
-        <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
+        <div className="rounded-full p-5 shadow-lg cursor-pointer hover:scale-110 hover:bg-blue-100 duration-300">
           <a 
             href="mailto:james2012.shun@gmail.com"
             title="Email">
-            <AiOutlineMail size={34} />
+            <AiOutlineMail size={22} />
           </a>
         </div>
-        <div className="rounded-sm p-2 hover:scale-110 hover:bg-yellow-200 transition">
+        <div className="rounded-full p-5 shadow-lg cursor-pointer hover:scale-110 hover:bg-blue-100 duration-300">
           <a 
             href="myResume.pdf"
             target="_blank" 
             rel="noopener noreferrer"
             title="Resume">
-            <AiOutlineSolution size={34} />
+            <AiOutlineSolution size={22} />
           </a>
         </div>
       </div>
