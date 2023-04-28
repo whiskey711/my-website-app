333cd15c43e552e5926270d6237fea701be93c45
临风 许 <WarMachine2012@outlook.com>
2023-04-27 21:50:33 -0700

    solve hook errors

--- pages/projects/games/blackjack.js
+++ pages/projects/games/blackjack.js
@@ -1,18 +1,7 @@
-import BlackjackInterface from "@/components/poker-components/BlackjackInterface";
-import BlackjackCover from "@/components/poker-components/BlackjackCover";
-import { useState } from "react";
+import BlackjackPage from "@/components/poker-components/BlackjackPage";
 
 export default function blackjack() {
-  const [start, setStart] = useState(false);
-
-  function startGame() {
-    setStart(true);
-  }
-
   return (
-    <div>
-      {!start && <BlackjackCover startGame={startGame} />}
-      {start && <BlackjackInterface />}
-    </div>
+    <BlackjackPage />
   );
 }
